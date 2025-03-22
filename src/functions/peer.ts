5c32fe3bfa591b5c0423ed195e9285a3dd3fde25
Abishek khadka <abishek1234khadka@gmail.com>
2025-03-23 02:47:34 +0545

    init

---
+++ src/functions/peer.ts
@@ -0,0 +1,11 @@
+class PeerConnection {
+
+static peer: RTCPeerConnection | null = null; 
+
+
+
+
+
+
+
+}
---
+++ src/functions/socket.ts
@@ -0,0 +1,60 @@
+import { io, Socket } from "socket.io-client";
+
+
+export class SocketSingleton {
+  private static instance: Socket | null = null;
+  private static isActive: boolean = false;
+
+  private constructor() {}
+
+  static getInstance(user : string) {
+    if (!SocketSingleton.instance ) {
+      SocketSingleton.instance = io("http://localhost:4000", {
+       query:{
+        user 
+       },
+        transports: ["websocket", "polling"],
+        
+      }
+      );
+      SocketSingleton.active();
+    }
+    
+    return SocketSingleton.instance;
+  }
+
+  static getStatus() {
+    return SocketSingleton.isActive;
+  }
+
+
+  static TheInstance (){
+    return SocketSingleton.instance
+  }
+  static active() {
+    SocketSingleton.isActive = true;
+  }
+
+  static disconnect() {
+    if (SocketSingleton.instance) {
+      SocketSingleton.instance.disconnect();
+      console.log("Socket disconnected");
+      SocketSingleton.instance = null;
+      SocketSingleton.isActive = false;
+    }
+  }
+
+  static emitEvent(eventName: string, data: unknown) {
+    if (!this.instance) {
+      console.error("Unable to connect to socket server");
+      return;
+    }
+    this.instance?.emit(eventName, data, (response: unknown) => {
+      console.log(response);
+    });
+  }
+
+  static connectSocket(token : string ) {
+    return SocketSingleton.getInstance(token);
+  }
+}
